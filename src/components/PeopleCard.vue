1718ad14b8ca455312022de8dc303bcb7f7e99d5
VerbaViktorJedlik <verba.viktor@students.jedlik.eu>
2024-12-13 14:05:30 +0100

    listazas megvan

---
+++ src/components/PeopleCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import type People from '@/types/People';
+const prop = defineProps(['p']);
+
+</script>
+
+<template>
+
+   <div class="card" style="width: 16rem;">
+      <img class="card-img-top" :src="prop['p'].avatar" alt="Card image cap">
+      <div class="card-body">
+         <h5 class="card-title">{{ prop['p'].first_name }} {{ prop['p'].last_name }}</h5>
+         <h6 class="card-text">{{ prop['p'].email }}</h6>
+      </div>
+   </div>
+</template>
+
+
+<style scoped></style>
---
+++ src/views/PeopleList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import PeopleCard from '@/components/PeopleCard.vue';
+import data_people from '@/data/data_people';
+import type People from '@/types/People';
+import { ref } from 'vue';
+
+const people = ref<People[]>();
+people.value = data_people.getPeople();
+
+</script>
+
+<template>
+  <h1 class="display-5">Emberek</h1>
+  <div class="row">
+    <PeopleCard v-for="p in people" :p="p" class="m-2">
+    </PeopleCard>
+  </div>
+</template>
+
+
+<style scoped></style>
